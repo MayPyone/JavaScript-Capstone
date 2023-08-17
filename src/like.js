a9c5560a8096b885eba089f0b5c8a37f1e2c47f0
May Pyone <maypyone015@gmail.com>
2023-08-17 18:06:13 +0630

    Create id

---
+++ src/like.js
@@ -0,0 +1,22 @@
+export const myid = 'gRf5RB5Zmrrnrx8W6GBe';
+export const url = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${myid}/likes/`;
+
+export const addlikes = (id) => {
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(
+      { item_id: id },
+    ),
+  })
+    .then((response) => response.json())
+    .catch((error) => console.log('ERRor'));
+};
+
+export const getLikes = async (id) => {
+  const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${myid}/likes/`);
+  const data = await response.json();
+  return data[id];
+};
