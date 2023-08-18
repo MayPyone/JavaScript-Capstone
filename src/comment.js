f474c9b59af0b679442f7d4279d974563b8ef617
Samuel Kinyanjui <sax80kinya@gmail.com>
2023-08-19 01:21:37 +0300

    export the get_comment
    export the get comments method

--- src/comment.js
+++ src/comment.js
@@ -14,3 +14,11 @@ export const create_comment = (id,name,comment)=> {
 })
   .then((response) => response.json())
 }
+export const get_comment = async (id) => {
+    const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/OOfjYu3VsRJgxqrLlAOO/comments?item_id=${id}`);
+    const data = await response.json();
+    return data;
+}
+
+ //create_comment(1,"shwe","nice");
+// get_comment(2);
