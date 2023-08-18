ea2ef7eac57587e92a40152a8b8d34ad9628321c
Samuel Kinyanjui <sax80kinya@gmail.com>
2023-08-18 14:53:37 +0300

    Update popup.js
    To get the button that triggers the event

--- src/popup.js
+++ src/popup.js
@@ -5,4 +5,13 @@ export const popup=(data,element)=>{
     const image = document.createElement('img');
     image.src = data.image.original;
     image.alt = data.name;
+    const container =document.querySelector('body');
+    const cross_button = document.createElement('button');
+    const cross_icon = document.createElement('i');
+    cross_button.classList.add('cross');
+    cross_icon.classList.add('fa-2x');
+    cross_icon.classList.add('fa-solid');
+    cross_icon.classList.add('fa-xmark');
+    const title = document.createElement('span');
+    title.classList.add('titlecount');
 }
