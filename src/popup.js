56596512e6d265223443869c6bb8b9b96beb23c6
Samuel Kinyanjui <sax80kinya@gmail.com>
2023-08-18 14:52:41 +0300

    Created the section to display the item
    created section for display

--- src/popup.js
+++ src/popup.js
@@ -1,4 +1,8 @@
 import { countitem, items } from './itemcount.js';
 export const popup=(data,element)=>{
-    
+    const section = document.createElement('section');
+    section.classList.add("section");
+    const image = document.createElement('img');
+    image.src = data.image.original;
+    image.alt = data.name;
 }
